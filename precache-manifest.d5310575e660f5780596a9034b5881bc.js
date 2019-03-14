e39590aae6685a4f3809129a0660abc8f900c4f7
kalsoom-zakria <kalsoom.zakria@miranz.net>
2019-03-14 12:37:11 +0500

    Updates

---
+++ precache-manifest.d5310575e660f5780596a9034b5881bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21deda1075bb265c5934",
+    "url": "/contact/static/js/runtime~main.197205d1.js"
+  },
+  {
+    "revision": "14c6874b6881758c9a1e",
+    "url": "/contact/static/js/main.cf909534.chunk.js"
+  },
+  {
+    "revision": "eff6b346f683ba05c393",
+    "url": "/contact/static/js/2.0a4c4ec4.chunk.js"
+  },
+  {
+    "revision": "14c6874b6881758c9a1e",
+    "url": "/contact/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "eff6b346f683ba05c393",
+    "url": "/contact/static/css/2.b3a6f8ea.chunk.css"
+  },
+  {
+    "revision": "8bd6ad18bb866fbcefc6f5ec3627f2da",
+    "url": "/contact/index.html"
+  }
+];
